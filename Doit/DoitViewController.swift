fca7a185626bc923380f48090b124d8ea05e43d7
Igor Tronin <tronin.88@gmail.com>
2019-08-04 14:01:55 +0300

    Add new item functionality complete.

--- Doit/DoitViewController.swift
+++ Doit/DoitViewController.swift
@@ -10,41 +10,55 @@ import UIKit
 
 class DoitViewController: UITableViewController {
 
-    let itemArray = ["Create a business idea", "Develop a monetization plan", "Set a milestones"]
-    
+    var itemArray = ["Create a business idea", "Develop a monetization plan", "Set a milestones"]
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-    //MARK - Tableview DataSource Methods
-    
-    
+    //MARK: Tableview DataSource Methods
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return itemArray.count
     }
-
-
+    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "DoitItemCell", for: indexPath)
-        
         cell.textLabel?.text = itemArray[indexPath.row]
-        
         return cell
     }
     
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        
         if tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark {
             tableView.cellForRow(at: indexPath)?.accessoryType = .none
         } else {
             tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
-            
         }
         tableView.deselectRow(at: indexPath, animated: true)
-        
     }
     
+
+    //MARK: Add new items
+    
+    @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
+        var textField = UITextField()
+        let alert = UIAlertController(title: "Добавить новую задачу", message: "", preferredStyle: .alert)
+        let action = UIAlertAction(title: "Добавить", style: .default) { (action) in
+            //what will happen whan user clicks the Add button on UIAlert
+            self.itemArray.append(textField.text!)
+            self.tableView.reloadData()
+        }
+        alert.addTextField { (alertTextField) in
+            alertTextField.placeholder = "Новая задача"
+            textField = alertTextField
+        }
+        alert.addAction(action)
+        present(alert, animated: true, completion: nil)
+    }
+    
+
 }
+
+
+
 
